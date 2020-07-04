5f5021126b8dd21961448dfca272572ea60740a5
Bryce B <bryce.boddy@gmail.com>
2020-07-04 05:37:35 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,9 +2,9 @@ class Ship < Pirate
   attr_accessor :name, :type, :booty
   @@all = []
   def initialize(params)
-    @name = params[:ship_name_1]
-    @type = params[:ship_type_1]
-    @booty = params[:ship_booty_1]
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
     @@all << self
   end
   
